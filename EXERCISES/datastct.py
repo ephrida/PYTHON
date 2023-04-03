82de993570522dd0cc963fd31b7a8212c85097aa
ephrida <92803292+ephrida@users.noreply.github.com>
2023-04-03 19:31:08 +0200

    strings and escape sequence

---
+++ EXERCISES/datastct.py
@@ -0,0 +1,23 @@
+'''data structures 
+lists
+dictionaries
+tuples
+sets
+a way of storing DeprecationWarninganything in a squre blankets
+and its separated by commas'''
+my_list=[1,2,3,4,5]
+#name= ['kim', 'ephrida']
+#people =['ephrida', 25,1.23]
+#print (my_list)
+#print(f'{my_list}')
+#print(f'{my_list[0]}')
+#for loop
+#for index in range(0,len(my_list)-1):
+    #print(f'index is {index}, value is {my_list[index]}')
+temperature_mzuzu=[21.3,26.5,8]
+#calculate the average
+sum=0
+for number in temperature_mzuzu:
+    number=number+1
+    sum=number
+    print(sum)
---
+++ EXERCISES/string.py
@@ -0,0 +1,14 @@
+'''name='ephrida Munthali wasv a student at MUST'
+school_Name=name.find('MUST')
+print(school_Name)'''
+#escape sequance
+#format specifiers
+#food = 'rice'
+'''print('my favourite food is:{}'.format(food))
+print (f'my favorite food is:{food}')'''
+#age=24
+#name='ephrida'
+#print ('my favorite food is:{0}  and kimalso like{0}and i am {1} years old'.format(food,age))
+#when you want to print the same thing more time you can use the above sentence
+PI=3.14159265359 
+print ('{:.2f}'.format(PI))
