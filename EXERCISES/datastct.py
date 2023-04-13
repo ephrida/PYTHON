81473c38d1d3fcc9c2ec700b09db1a657aa0bc75
ephrida <92803292+ephrida@users.noreply.github.com>
2023-04-13 19:56:02 +0200

    updated datastct.py

--- EXERCISES/datastct.py
+++ EXERCISES/datastct.py
@@ -5,7 +5,7 @@ tuples
 sets
 a way of storing DeprecationWarninganything in a squre blankets
 and its separated by commas'''
-my_list=[1,2,3,4,5]
+#my_list=[1,2,3,4,5]
 #name= ['kim', 'ephrida']
 #people =['ephrida', 25,1.23]
 #print (my_list)
@@ -14,10 +14,17 @@ my_list=[1,2,3,4,5]
 #for loop
 #for index in range(0,len(my_list)-1):
     #print(f'index is {index}, value is {my_list[index]}')
-temperature_mzuzu=[21.3,26.5,8]
+#temperature_mzuzu = [21.3,26.5,8]
 #calculate the average
-sum=0
-for number in temperature_mzuzu:
-    number=number+1
-    sum=number
-    print(sum)
+'''sum = 0
+size = len(temperature_mzuzu)
+for i in range (0,size):
+    sum =sum+temperature_mzuzu[i]
+print(sum)
+average =sum/size 
+print(f'the average is;{average}')
+print ('the avearge is:{:.2f}'.format (average))'''
+temperature_mzuzu=[21.3,26.5,8]
+temperature_mzuzu.append(23)
+temperature_mzuzu.insert(0,23.2)
+print(f'the added number is:{temperature_mzuzu}')
